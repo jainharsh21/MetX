64c8dc16076ce1b534404b2f70394fe852fb7c3e
HARSH JAIN <jainharsh270@gmail.com>
2021-05-07 15:24:16 +0530

    landing and profile added for student chapter

---
+++ lib/screens/student_chapter/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StudentChapterHome extends StatefulWidget {
+  final Map userData;
+  StudentChapterHome({this.userData});
+  @override
+  _StudentChapterHomeState createState() => _StudentChapterHomeState();
+}
+
+class _StudentChapterHomeState extends State<StudentChapterHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/screens/student_chapter/landing.dart
+++ lib/screens/student_chapter/landing.dart
@@ -1,5 +1,8 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:metx/constants/colors.dart';
+import 'package:metx/screens/student_chapter/home.dart';
+import 'package:metx/screens/student_chapter/profile.dart';
 
 class StudentChapterLanding extends StatefulWidget {
   final Map userData;
@@ -9,18 +12,43 @@ class StudentChapterLanding extends StatefulWidget {
 }
 
 class _StudentChapterLandingState extends State<StudentChapterLanding> {
+  final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          onTap: () async {
-            SharedPreferences prefs = await SharedPreferences.getInstance();
-            prefs.clear();
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: PageView(
+          controller: _pageController,
+          physics: NeverScrollableScrollPhysics(),
+          children: [
+            StudentChapterHome(
+              userData: widget.userData,
+            ),
+            StudentChapterProfile(
+              userData: widget.userData,
+            ),
+          ],
+          onPageChanged: (index) {
+            setState(() {
+              _pageController.jumpToPage(index);
+            });
           },
-          child: Text(
-            widget.userData.toString(),
-          ),
+        ),
+        bottomNavigationBar: CurvedNavigationBar(
+          color: utilityColor,
+          backgroundColor: mainThemeColor,
+          items: <Widget>[
+            Icon(Icons.home, size: 30),
+            Icon(Icons.person),
+          ],
+          height: 60.0,
+          onTap: (index) {
+            setState(() {
+              _pageController.jumpToPage(index);
+            });
+          },
         ),
       ),
     );
