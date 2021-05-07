525d4277467a91c5a561815e6f2db8f6d44407f1
HARSH JAIN <jainharsh270@gmail.com>
2021-05-07 15:31:43 +0530

    added student chapter home and navigation to create event

---
+++ lib/screens/student_chapter/create_event.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateEvent extends StatefulWidget {
+  final Map userData;
+  CreateEvent({this.userData});
+  @override
+  _CreateEventState createState() => _CreateEventState();
+}
+
+class _CreateEventState extends State<CreateEvent> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/screens/student_chapter/home.dart
+++ lib/screens/student_chapter/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:metx/constants/colors.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'create_event.dart';
+import '../welcome.dart';
 
 class StudentChapterHome extends StatefulWidget {
   final Map userData;
@@ -10,6 +15,51 @@ class StudentChapterHome extends StatefulWidget {
 class _StudentChapterHomeState extends State<StudentChapterHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        backgroundColor: utilityColor,
+        centerTitle: true,
+        title: Text(
+          "MetX",
+          style: GoogleFonts.stylish(),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () async {
+              SharedPreferences prefs = await SharedPreferences.getInstance();
+              prefs.clear();
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Welcome(),
+                  ),
+                  (route) => false);
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "Student Chapter Home",
+          style: GoogleFonts.lato(
+            fontSize: 20.0,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: mainThemeColor,
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CreateEvent(),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
