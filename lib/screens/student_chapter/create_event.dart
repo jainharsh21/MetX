6830ce1cb2d0a8a1612a1b23061daab6d978a674
DotSyndicate <dotsyndicateagency@gmail.com>
2022-04-30 23:31:23 +0530

    added image picking and completed event image uploading

--- lib/screens/student_chapter/create_event.dart
+++ lib/screens/student_chapter/create_event.dart
@@ -1,10 +1,15 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:metx/constants/colors.dart';
 import 'package:metx/screens/student_chapter/landing.dart';
 import 'package:metx/utils/apiCaller.dart';
 import 'package:metx/utils/form_validators.dart';
+import 'package:uuid/uuid.dart';
 
 class CreateEvent extends StatefulWidget {
   final Map userData;
@@ -21,6 +26,107 @@ class _CreateEventState extends State<CreateEvent> {
   TextEditingController _dateController = TextEditingController();
   TextEditingController _feesController = TextEditingController(text: "0");
   String eventDate = '';
+
+  File file;
+  final _picker = ImagePicker();
+  bool isUploading = false;
+  String eventUrl = '';
+
+  handleTakePhoto() async {
+    // to take the photo from the camera.
+    Navigator.pop(context);
+    XFile file = await _picker.pickImage(
+      source: ImageSource.camera,
+      maxHeight: 900,
+      maxWidth: 960,
+    );
+    if (file != null) {
+      setState(() {
+        this.file = File(file.path);
+      });
+    }
+  }
+
+  handleImageFromGallery() async {
+    Navigator.pop(context);
+    XFile file = await _picker.pickImage(source: ImageSource.gallery);
+    if (file != null) {
+      setState(() {
+        this.file = File(file.path);
+      });
+    }
+  }
+
+  selectEventPic(parentContext) {
+    return showDialog(
+        context: parentContext,
+        builder: (context) {
+          return SimpleDialog(
+            elevation: 45.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            title: Text(
+              "Choose Event Picture",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            children: <Widget>[
+              SimpleDialogOption(
+                child: Text(
+                  "Image With Camera",
+                  style: TextStyle(),
+                ),
+                onPressed: handleTakePhoto,
+              ),
+              SimpleDialogOption(
+                child: Text(
+                  "Image From Gallery",
+                  style: TextStyle(),
+                ),
+                onPressed: handleImageFromGallery,
+              ),
+              SimpleDialogOption(
+                child: Text(
+                  "Cancel",
+                  style: TextStyle(),
+                ),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        });
+  }
+
+  // ignore: missing_return
+  Future<String> uploadImageToFirebase(imageFile) async {
+    String url = "";
+    FirebaseStorage storage = FirebaseStorage.instance;
+    var id = Uuid().v4();
+    Reference ref = storage.ref().child("event-images/$id");
+    UploadTask uploadTask = ref.putFile(file);
+    await uploadTask.whenComplete(() async {
+      url = await ref.getDownloadURL();
+    });
+    return url;
+  }
+
+  uploadImage() async {
+    setState(() {
+      isUploading = true;
+    });
+    String imageUrl = await uploadImageToFirebase(file);
+    setState(() {
+      isUploading = false;
+      eventUrl = imageUrl;
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Image Uploaded!"),
+      ),
+    );
+  }
 
   final _formKey = GlobalKey<FormState>();
 
@@ -157,6 +263,57 @@ class _CreateEventState extends State<CreateEvent> {
                     ),
                   ),
                 ),
+                SizedBox(height: 10),
+                Text("Upload Event Iamge"),
+                SizedBox(height: 10),
+                Card(
+                  child: Container(
+                    color: Colors.white,
+                    height: 200,
+                    child: Center(
+                      child: GestureDetector(
+                        onTap: () {
+                          selectEventPic(context);
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: file != null
+                                ? DecorationImage(
+                                    fit: BoxFit.fill, image: FileImage(file))
+                                : null,
+                            color: Colors.blueGrey,
+                          ),
+                          child: Center(
+                            child: file == null
+                                ? Icon(
+                                    Icons.camera_alt,
+                                    size: 58.0,
+                                    color: Colors.white,
+                                  )
+                                : SizedBox.shrink(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: ElevatedButton.icon(
+                    icon: Icon(Icons.upload),
+                    style: ElevatedButton.styleFrom(
+                      primary: mainThemeColor,
+                    ),
+                    onPressed: file != null
+                        ? () async {
+                            await uploadImage();
+                          }
+                        : null,
+                    label: Text(
+                      "Upload Image",
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
                 SizedBox(
                   height: 20.0,
                 ),
@@ -173,7 +330,7 @@ class _CreateEventState extends State<CreateEvent> {
                     onPressed: () async {
                       Map eventData = {
                         "name": _nameController.text,
-                        "event_img_url": "testurl",
+                        "event_img_url": eventUrl,
                         "description": _descriptionController.text,
                         "summary": _summaryController.text,
                         "event_at": eventDate,
